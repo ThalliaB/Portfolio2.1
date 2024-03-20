7c5a1be56ef90ff520fc24be8b6de3bea82ad3a8
ThalliaB <thalliabrum60@gmail.com>
2024-03-20 14:01:55 -0300

    Add new project in experience page

--- src/components/pages/Experience.js
+++ src/components/pages/Experience.js
@@ -1,3 +1,4 @@
+import img0 from '../../img/imgProject/MyLibrary.png'
 import img1 from '../../img/imgProject/LetYourComment.jpg'
 import img2 from '../../img/imgProject/wildbest.jpg'
 import img3 from '../../img/imgProject/AluraFlix.jpg'
@@ -12,12 +13,21 @@ function Experience() {
         <section className={styles.experience}>
 
             <h1 className={styles.h1Experience}>Meus projetos</h1>
-            <p style={{textAlign: 'justify', textIndent: '1em'}}>Sempre gostei muito de utilizar HTML e CSS então uma parte dos meus projetos é baseado nisso, mas com o passar dos meses, me surgiu o desejo de fazer coisas mais elaboradas e bonitas.</p>
-            <p style={{textAlign: 'justify', textIndent: '1em'}}>Foi difícil até que consegui fazer algumas das coisas que queria, pois não tinha tanta prática e não entendia bem como funcionavam, mas com muita persistência, de pouco em pouco fui construindo o que tenho hoje. Muitas dessas aplicações no começo eram coisas tão simples que se for olhar eram tão feias comparadas com as de hoje. Mas felizmente eu nunca desisti de tentar.</p>
-            <p style={{textAlign: 'justify', textIndent: '1em'}}>Tudo o que eu sei hoje, eu aprendi sozinha, então tenho muito orgulho das coisas que fiz e das barreiras que atravessei, pois eu sei o quanto foi difícil, antes que tudo se tornasse claro para mim. HTML, CSS e JavaScript foi um "divisor de águas" para mim. Já ReactJS foi algo que me permitiu colocar em prática o que eu já sabia e um pouco mais. Java, Python e MySQL me fizeram ver o que eu ainda tenho a frente para aprender, e é isso que me dá forças todos os dias para seguir firme apesar dos erros que acontecem.</p>
-            <p style={{paddingBottom: '1em', textAlign: 'justify', textIndent: '1em'}}>Então aqui estão alguns de meus projetos:</p>
+            <p style={{ textAlign: 'justify', textIndent: '1em' }}>Sempre gostei muito de utilizar HTML e CSS então uma parte dos meus projetos é baseado nisso, mas com o passar dos meses, me surgiu o desejo de fazer coisas mais elaboradas e bonitas.</p>
+            <p style={{ textAlign: 'justify', textIndent: '1em' }}>Foi difícil até que consegui fazer algumas das coisas que queria, pois não tinha tanta prática e não entendia bem como funcionavam, mas com muita persistência, de pouco em pouco fui construindo o que tenho hoje. Muitas dessas aplicações no começo eram coisas tão simples que se for olhar eram tão feias comparadas com as de hoje. Mas felizmente eu nunca desisti de tentar.</p>
+            <p style={{ textAlign: 'justify', textIndent: '1em' }}>Tudo o que eu sei hoje, eu aprendi sozinha, então tenho muito orgulho das coisas que fiz e das barreiras que atravessei, pois eu sei o quanto foi difícil, antes que tudo se tornasse claro para mim. HTML, CSS e JavaScript foi um "divisor de águas" para mim. Já ReactJS foi algo que me permitiu colocar em prática o que eu já sabia e um pouco mais. Java, Python e MySQL me fizeram ver o que eu ainda tenho a frente para aprender, e é isso que me dá forças todos os dias para seguir firme apesar dos erros que acontecem.</p>
+            <p style={{ paddingBottom: '1em', textAlign: 'justify', textIndent: '1em' }}>Então aqui estão alguns de meus projetos:</p>
 
             <section className={styles.cards}>
+                <Card
+                    img={img0} alt=''
+                    title='My Library'
+                    p='Programa que lista os livros que tenho na estante'
+                    tag='#Csharp #C# #CSS #JS'
+                    link={`https://github.com/ThalliaB/MyLibrary`}
+                    link2={`https://github.com/ThalliaB/MyLibrary`}
+                />
+
                 <Card
                     img={img1} alt=''
                     title='LetYourComment'
@@ -44,7 +54,7 @@ function Experience() {
                     link={`https://github.com/ThalliaB/AluraFlix`}
                     link2={`https://thalliab.github.io/AluraFlix/`}
                 />
-                
+
                 <Card
                     img={img4} alt=''
                     title='Fruta e Fruto'
@@ -53,7 +63,7 @@ function Experience() {
                     link={`https://github.com/ThalliaB/projet-Fruta-Fruto`}
                     link2={`https://projeto-fruta-fruto.vercel.app/`}
                 />
-                
+
                 <Card
                     img={img5} alt=''
                     title='Top Casa Fina Arquitetura'
@@ -62,7 +72,7 @@ function Experience() {
                     link={`https://github.com/ThalliaB/CasaFina-Arquitetura-css`}
                     link2={`https://casa-fina-arquitetura-css.vercel.app/`}
                 />
-                
+
                 <Card
                     img={img6} alt=''
                     title='Barbearia Alura'
